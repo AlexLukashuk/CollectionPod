99c8578f76049ae35708215049d4750f335eb142
Oleksandr Lukashuk <oleksandr.lukashuk@onseo.biz>
2021-08-25 12:21:51 +0300

    Initial Commit

---
+++ CollectionPod/CollectionPod.h
@@ -0,0 +1,18 @@
+//
+//  CollectionPod.h
+//  CollectionPod
+//
+//  Created by Oleksandr Lukashuk on 25.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CollectionPod.
+FOUNDATION_EXPORT double CollectionPodVersionNumber;
+
+//! Project version string for CollectionPod.
+FOUNDATION_EXPORT const unsigned char CollectionPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionPod/PublicHeader.h>
+
+
